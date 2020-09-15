ceaf244d8dec58ca3553b09988d40fafa4c274ff
fabyxipe <fabien.rahajaniaina@xipe.com>
2020-09-16 00:49:12 +0200

    set input and select style

--- app/src/backacess/pages/accounts/details/content.jsx
+++ app/src/backacess/pages/accounts/details/content.jsx
@@ -40,7 +40,7 @@ const Content = function(){
 								/>
 						</div>
 						 <div className="form1 tb-cell">
-								<label className="form3-label display vm" htmlFor="paie">Paie les loyers:</label>
+								<label className="form3-label display vm" htmlFor="paie">Paie les loyers :</label>
 								<input type="checkbox"
 									className="display vm"
 									id="paie" name="loyers"
@@ -48,101 +48,118 @@ const Content = function(){
 									/>
 						  </div>
 						<div className="form1 tb-cell">
-								<label className="form-check-label display vm" htmlFor="offre">Offre Assureurs:</label>
+								<label className="form-check-label display vm" htmlFor="offre">Offre Assureurs : </label>
 								<input type="checkbox"
 									className="display vm"
 									id="offre" name="assureur" />
 						</div>
 						<div className="form1 scoring">
 						  	<label htmlFor="scoring">Scoring (mail au changement) : </label>
-						    <select className="border pad5" id="scoring" name="scoring" ref={register()}>
+						    <select className="noborder bs opt" id="scoring" name="scoring" ref={register()}>
 						      <option value="traite">Traité</option>
 						      <option value="non">Non-traité</option>
 						    </select>
 						</div>
 					</div>
-					<div className="form-info bs not-bs">
+					<div className="form-info pad13 bs not-bs">
 						<div className="field">
 							<label>Genre</label><br/>
-							<input type="text" name="" placeholder=""/>
+							<select className="opt noborder bs">
+								<option>Homme</option>
+								<option>Femme</option>
+							</select>
 						</div>
 						<div className="field">
 							<label>Nom</label><br/>
-							<input type="text" name="" placeholder=""/>
+							<input type="text" name="" className="noborder bs" placeholder=""/>
 						</div>
 						<div className="field">
 							<label>Prénom</label><br/>
-							<input type="text" name="" placeholder=""/>
+							<input type="text" className="noborder bs" name="" placeholder=""/>
 						</div>
 						<div className="field">
 							<label>E-mail</label><br/>
-							<input type="text" className="email-field" name="" placeholder=""/>
+							<input type="text" className="noborder bs email-field" name="" placeholder=""/>
 						</div>
 						<div className="info">
 							<div className="info-supp">
 								<label>Téléphone</label><br/>
-								<input type="text" name="" placeholder=""/>
+								<input type="text" name="" className="noborder bs" placeholder=""/>
 							</div>
 							<div className="info-supp">
 								<label>Adresse</label><br/>
-								<input type="text" name="" placeholder=""/>
+								<input type="text" name="" className="noborder bs" placeholder=""/>
 							</div>
 							<div className="info-supp">
 								<label>Ville & CP</label><br/>
-								<input type="text" name="" placeholder=""/>
+								<input type="text" name="" className="noborder bs" placeholder=""/>
 							</div>
 						</div>
 					</div>
-					<div className="form-info bs not-bs">
-						<div className="info-bq d-flex">
+					<div className="form-info bs pad13 not-bs">
+						<div className="info-in1">
 							<div className="info-bq-detail">
-							    <label htmlFor="banque">Banque</label>
-							    <select className="" id="banque" name="banque" ref={register()}>
+							    <label htmlFor="banque">Banque</label><br/>
+							    <select className="noborder bs opt" id="banque" name="banque" ref={register()}>
 							      <option value="hsbc">HSBC</option>
 							      <option value="bfv">BFV</option>
 							      <option value="credit">CREDIT AGRICOLE</option>
 							    </select>
 							</div>
 							<div className="info-bq-detail">
-							    <label htmlFor="anciennete">Ancienneté</label>
-							    <select className="" id="anciennete" name="anciennete" ref={register()}>
+							    <label htmlFor="anciennete">Ancienneté</label><br/>
+							    <select className="noborder bs opt" id="anciennete" name="anciennete" ref={register()}>
 							      <option value="2018">2018</option>
 							      <option value="2017">2017</option>
 							    </select>
 							</div>
 							<div className="info-bq-detail">
-							    <label htmlFor="situation">Situation permis</label>
-							    <select className="" id="situation" name="situation" ref={register()}>
+							    <label htmlFor="situation">Situation permis</label><br/>
+							    <select className="noborder bs opt" id="situation" name="situation" ref={register()}>
 							      <option value="2ans">2 ans ou plus jamais suspendu</option>
 							      <option value="3ans">3 ans</option>
 							    </select>
 							</div>
 						</div>
+						<div className="info-in2">
+							<div className="info-bq-detail">
+							    <label htmlFor="banque">Situation assurance</label><br/>
+							    <select className="noborder opt bs" id="banque" name="banque" ref={register()}>
+							      <option value="sinistre">0 ou 1 sinistre en 3 ans</option>
+							      <option value="sinistre">0 ou 1 sinistre en 3 ans</option>
+							      <option value="sinistre">0 ou 1 sinistre en 3 ans</option>
+							    </select>
+							</div>
+							<div className="info-bq-detail">
+							    <label>Encours</label><br/>
+								<input type="text" name="" className="noborder bs opt" placeholder=""/>
+							</div>
+						</div>
 					</div>
-					<div className="form-info bs not-bs row info-source">
-						<div className="col-sm-4">
-							<label htmlFor="og_source">OG_Source</label>
+					<div className="form-info bs pad13 not-bs info-source">
+						<div className="display">
+							<label htmlFor="og_source">OG_Source</label><br/>
 							<input type="text"
-								className=""
+								className="noborder bs opt"
 								id="og_source"
 								placeholder=""
 								name="source"
 								ref={register()}
 							/>
 						</div>
-							<div className="col-sm-4">
-								<label htmlFor="cclid">CClid</label>
+							<div className="display">
+								<label htmlFor="cclid">CClid</label><br/>
 								<input type="text"
-									className=""
+									className="noborder bs opt"
 									id="cclid" placeholder=""
 									ref={register()}
 									name="cclid"
 								/>
 							</div>
-							<div className="col-sm-3 urlss">
-								<label htmlFor="url_source">URL_Source</label>
+							<div className="display urlss">
+								<label htmlFor="url_source">URL_Source</label><br/>
 								<input type="text"
-									className=""
+									className="noborder bs opt"
 									id="url_source" placeholder=""
 									name="url"
 									ref={register()}
@@ -150,7 +167,7 @@ const Content = function(){
 							</div>
 					</div>
 					<div className="not-bs" style={{"fontWeight":"bold"}}>
-						MODIFIER MON MOT DE PASSE
+						<h2>Modifier mot de passe</h2>
 					</div>
 					<ChangePwd />
 					<div className="not-bs save">
